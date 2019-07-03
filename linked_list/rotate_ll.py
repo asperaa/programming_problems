4dac9f62d4669a4b1f6ed148c9ada627ec320a27
asperaa <adityaankur44@gmail.com>
2019-07-03 11:56:36 +0530

    wip: more boiler plate code for rotate linked list

--- linked_list/rotate_ll.py
+++ linked_list/rotate_ll.py
@@ -4,3 +4,26 @@ class Node:
         self.data = data
         self.next = None
 
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+    
+    def add_front(self, data):
+        
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        new_node.next = self.head
+        self.head = new_node
+    
+    def print_list(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+    
