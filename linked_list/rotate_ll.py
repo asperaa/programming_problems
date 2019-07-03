4d2f5af2c0c4bb47e763b20fc4680af984e03a26
asperaa <adityaankur44@gmail.com>
2019-07-03 12:19:28 +0530

    ll: rotate a linked list by k nodes

--- linked_list/rotate_ll.py
+++ linked_list/rotate_ll.py
@@ -5,10 +5,12 @@ class Node:
         self.next = None
 
 class LinkedList:
-
+    
+    # add a node to the end of linked list
     def __init__(self):
         self.head = None
     
+    # add a node to the front of linked list
     def add_front(self, data):
         
         new_node = Node(data)
@@ -20,6 +22,7 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
     
+    # print a linked list
     def print_list(self):
         temp = self.head
         while(temp):
@@ -27,3 +30,42 @@ class LinkedList:
             temp = temp.next
         print()
     
+    
+    # rotate a linked list by k node in counter clock wise direction
+    def rotate(self, k):
+        # check if the ll is empty
+        if self.head is None:
+            return
+        
+        counter = 0
+        mover = self.head
+        prev = self.head
+        while(mover and counter != k):
+            counter += 1
+            prev = mover
+            mover = mover.next
+        # mark the next of last node of the new ll as None
+        prev.next = None 
+        # save the new head of the linked list
+        new_head = mover
+        # reach the last node of the current linked list
+        while(mover.next):
+            mover = mover.next
+        # next of the last node of current ll to the head of current ll
+        mover.next = self.head
+        self.head = new_head
+    
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.add_front(60)
+    ll.add_front(50)
+    ll.add_front(40)
+    ll.add_front(30)
+    ll.add_front(20)
+    ll.add_front(10)
+
+    ll.print_list()
+
+    ll.rotate(4)
+
+    ll.print_list()
