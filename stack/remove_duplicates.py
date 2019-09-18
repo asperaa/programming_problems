4949ec882175cbd7d77a0936bf37df70e32b09b4
asperaa <adityaankur44@gmail.com>
2019-09-18 19:18:35 +0530

    problem: remove duplicate letters maintain lexicographical

---
+++ stack/remove_duplicates.py
@@ -0,0 +1,19 @@
+"""Remove duplicates and maintain lexicographical sequence"""
+def remove_dup(arr):
+    stack = []
+    seen = set()
+    last_occurence = {element: i for i, element in enumerate(arr)}
+
+    for i, element in enumerate(arr):
+        if element not in seen:
+            while stack and element < stack[-1] and i < last_occurence[stack[-1]]:
+                seen.discard(stack.pop())
+            stack.append(element)
+            seen.add(element)
+    return "".join(stack)
+
+if __name__ == "__main__":
+    arr = "cbacdbc"
+    print(remove_dup(arr))
+
+        
