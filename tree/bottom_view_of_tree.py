c7df63b49a271c862a3ac90413f844ddd1b3e3a8
asperaa <adityaankur44@gmail.com>
2019-10-12 14:31:07 +0530

    problem: top view of the tree correction

--- tree/bottom_view_of_tree.py
+++ tree/bottom_view_of_tree.py
@@ -12,33 +12,34 @@ class BottomView:
         self.minn = 0
         self.node_hd = {}
     
-def bottom_view_util(self, root, hd):
-    if not root:
-        return None
-    self.node_hd[root] = hd
-    self.maxx = max(self.maxx, hd)
-    self.minn = min(self.minn, hd)
-    if hd not in self.hd_dict:
-        self.hd_dict[hd] = [root.val]
-    else:
-        self.hd_dict[hd].append(root.val)
-    self.bottom_view_util(root.left, hd - 1)
-    self.bottom_view_util(root.right, hd + 1)
+    def bottom_view_util(self, root, hd):
+        if not root:
+            return None
+        self.node_hd[root] = hd
+        self.maxx = max(self.maxx, hd)
+        self.minn = min(self.minn, hd)
+        if hd not in self.hd_dict:
+            self.hd_dict[hd] = [root.val]
+        else:
+            self.hd_dict[hd].append(root.val)
+        self.bottom_view_util(root.left, hd - 1)
+        self.bottom_view_util(root.right, hd + 1)
     
     def bottom_view(self, root):
         self.bottom_view_util(root, 0)
         result = []
         for i in range(self.minn, self.maxx + 1):
+            print(self.hd_dict[i])
             result.append(self.hd_dict[i][-1])
         return result
 if __name__ == "__main__":
     root = TreeNode(1)
     root.left = TreeNode(2)
     root.right = TreeNode(3)
-    root.left.left = TreeNode(4)
-    root.left.right = TreeNode(5)
-    root.right.left = TreeNode(6)
-    root.right.right = TreeNode(7)
+    root.left.right = TreeNode(4)
+    root.left.right.right = TreeNode(5)
+    root.left.right.right.right = TreeNode(6)
+    root.left.right.right.right.right = TreeNode(7)
     BV = BottomView()
 
     print(BV.bottom_view(root))
--- tree/top_view_of_tree.py
+++ tree/top_view_of_tree.py
@@ -1,4 +1,5 @@
 """Top view of a tree"""
+from collections import deque
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -12,32 +13,42 @@ class TopView:
         self.minn = 0
         self.hd_dict = {}
     
-    def top_view_util(self, root, hd):
+    def top_view(self, root):
         if not root:
             return None
-        root.hd = hd
-        self.maxx = max(self.maxx, hd)
-        self.minn = min(self.minn, hd)
-        if hd not in self.hd_dict:
-            self.hd_dict[root.hd] = root.val 
-        self.top_view_util(root.left, hd - 1)
-        self.top_view_util(root.right, hd + 1)
-    
-    def top_view(self, root):
-        self.top_view_util(root, 0)
         result = []
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            node = queue.popleft()
+            self.maxx = max(self.maxx, node.hd)
+            self.minn = min(self.minn, node.hd)
+            if node.hd not in self.hd_dict:
+                self.hd_dict[node.hd] = node.val
+            if node.left:
+                node.left.hd = node.hd - 1
+                queue.append(node.left)
+            if node.right:
+                node.right.hd = node.hd + 1
+                queue.append(node.right) 
+        
         for i in range(self.minn, self.maxx + 1):
             result.append(self.hd_dict[i])
         return result
+        
+        
+    
+
 
 if __name__ == "__main__":
     root = TreeNode(1)
     root.left = TreeNode(2)
     root.right = TreeNode(3)
-    root.left.left = TreeNode(4)
-    root.left.right = TreeNode(5)
-    root.right.left = TreeNode(6)
-    root.right.right = TreeNode(7)
+    root.left.right = TreeNode(4)
+    root.left.right.right = TreeNode(5)
+    root.left.right.right.right = TreeNode(6)
+    root.left.right.right.right.right = TreeNode(7)
     TV = TopView()
 
     print(TV.top_view(root))
