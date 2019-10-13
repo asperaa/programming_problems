73f9f01dc9dab0729dba108260da7443c532f7df
asperaa <adityaankur44@gmail.com>
2019-10-13 18:00:36 +0530

    problem: connect nodes in a tree constant space

---
+++ tree/connect_nodes_of _tree_in_constant_space.py
@@ -0,0 +1,35 @@
+"""Connect nodes of tree in constant space"""
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.next = None
+
+def connect(root):
+        original_root = root
+        tail = TreeNode(0)
+        dummy = tail
+        while root:
+            tail.next = root.left
+            if tail.next:
+                tail = tail.next
+            tail.next = root.right
+            if tail.next:
+                tail = tail.next
+            root = root.next
+            if root == None:
+                tail = dummy
+                root = dummy.next
+        return original_root
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.right.right = TreeNode(5)
+    connect_nodes(root)
+    print(root.left.left.next.val) 
+
+        
