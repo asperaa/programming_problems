bac920c124b404730fafe88500bfb92a74669029
asperaa <adityaankur44@gmail.com>
2019-10-20 19:41:23 +0530

    comments: invert a binary tree

--- tree/invert_tree.py
+++ tree/invert_tree.py
@@ -7,34 +7,24 @@ class TreeNode:
     
 def invert_tree(root):
 
-
-    if root.left and root.right:
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        invert_tree(root.left)
-        invert_tree(root.right)
-    elif root.left and not root.right:
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        invert_tree(root.right)
-    elif root.right and not root.left:
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        invert_tree(root.left)
+    if not root:
+        return None
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+    invert_tree(root.left)
+    invert_tree(root.right)
     return root
 
 if __name__ == "__main__":
     root = TreeNode(4)
     root.left = TreeNode(2)
-    # root.right = TreeNode(7)
-    # root.left.left = TreeNode(1)
-    # root.left.right = TreeNode(3)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
     # root.right.left = TreeNode(6)
     # root.right.right = TreeNode(9)
 
     root = invert_tree(root)
-    print(root.right.val) 
+    print(root.right.right.val) 
 
