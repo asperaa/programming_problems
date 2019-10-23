13233d131020a209170514038576841dfa9d12d9
asperaa <adityaankur44@gmail.com>
2019-10-23 21:46:09 +0530

    comments: build the maximum binary tree. O(n) and O(n)

--- tree/maximum_binary_tree_3.py
+++ tree/maximum_binary_tree_3.py
@@ -6,18 +6,29 @@ class TreeNode:
         self.right = None
 
 def max_tree_linear(arr):
+    # maintain a stack which always has nums in descending order
+    # (bottom of stack will have largest number)
     stack = []
+    # this will store the element that should be left of the root
     last = None
+    # iterate the array from left to right
     for num in arr:
+        # pop the elements from stack which are smaller than the current array
         while stack and stack[-1].val < num:
             last = stack.pop()
+        # make tree-node fomr curr number
         node = TreeNode(num)
+        # assign the right child of top node in stack
         if stack:
             stack[-1].right = node
+        # assign the left node of the curr node (curr num in the array)
         if last:
             node.left = last
+        # append the curr node
         stack.append(node)
+        # reset the last pointer
         last = None
+    # in the end the stack will have the largest number of the array (which is root of the built tree)
     return stack[0]
 
 if __name__  == "__main__":
