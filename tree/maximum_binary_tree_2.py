53d77b8ec86594f6fc1996e92f844a710866dd27
asperaa <adityaankur44@gmail.com>
2019-10-23 13:16:11 +0530

    comments: build maximum BT. O(n2) and O(n)

--- tree/maximum_binary_tree_2.py
+++ tree/maximum_binary_tree_2.py
@@ -4,7 +4,8 @@ class TreeNode:
         self.val = val
         self.left = None
         self.right = None
-    
+
+# linear search to get the index of the array    
 def get_max_index(arr, l, r):
     max_index = l
     for i in range(l, r):
@@ -12,19 +13,25 @@ def get_max_index(arr, l, r):
             max_index = i
     return max_index
 
-
+# util function to apppy rexursion on array. Passing indices (left and right) to direct the recursion. 
 def max_tree_util(arr, l, r):
-    
+    # base case: when max index and left index are same or right index and max_index+1 are same
     if l == r:
         return None
+    # get the index of the max element in the array
     max_index = get_max_index(arr, l, r)
+    # make the node with the corresponding max value of array
     root = TreeNode(arr[max_index])
+    # make a recursive call to the left par the max index
     root.left = max_tree_util(arr, l, max_index)
+    # make arecursive call to the right part of the max index
     root.right = max_tree_util(arr, max_index+1, r)
     return root
 
+# main function to call the uti function with suitable indices (0 and length)
 def max_tree(arr):
     return max_tree_util(arr, 0, len(arr))
+
 if __name__  == "__main__":
     arr = [3, 2, 1, 6, 0, 5]
     root = max_tree(arr)
