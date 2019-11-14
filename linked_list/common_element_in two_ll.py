5e6b48af22dc63a5345349521ed45a18f4aae28b
asperaa <adityaankur44@gmail.com>
2019-11-15 00:56:44 +0530

    comments: common element in two intersectiing linked lists Time - O(m+n)

--- linked_list/common_element_in two_ll.py
+++ linked_list/common_element_in two_ll.py
@@ -1,6 +1,24 @@
 """find the first common elment in two linked list 
    i.e find the first node of the first list which is
-   also present in the second list"""
+   also present in the second list
+
+Approach 3: Two Pointers
+Maintain two pointers pApA and pBpB initialized at the head of A and B, respectively. 
+
+Then let them both traverse through the lists, one node at a time.
+
+When pApA reaches the end of a list, then redirect it to the head of B (yes, B, that's right.); 
+similarly when pBpB reaches the end of a list, redirect it the head of A.
+If at any point pApA meets pBpB, then pApA/pBpB is the intersection node.
+To see why the above trick would work, consider the following two lists: A = {1,3,5,7,9,11} 
+and B = {2,4,9,11}, which are intersected at node '9'. Since B.length (=4) < A.length (=6), 
+pBpB would reach the end of the merged list first, because pBpB traverses exactly 2 nodes less than pApA does. 
+By redirecting pBpB to head A, and pApA to head B, we now ask pBpB to travel exactly 2 more nodes than pApA would. 
+So in the second iteration, they are guaranteed to reach the intersection node at the same time.
+If two lists have intersection, then their last nodes must be the same one. 
+So when pApA/pBpB reaches the end of a list, record the last element of A/B respectively. 
+If the two last elements are not the same one, then the two lists have no intersections.   
+"""
 # Node class
 class Node:
     # initialise the node with data and next pointer
