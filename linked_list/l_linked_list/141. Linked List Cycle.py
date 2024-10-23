7cd8401d3db465917b5ad82eb71c1f75223167de
asperaa <adityaankur44@gmail.com>
2024-10-23 10:05:41 +0530

    141. Linked List Cycle

---
+++ linked_list/l_linked_list/141. Linked List Cycle.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow_ptr = fast_ptr = head
+        while fast_ptr and fast_ptr.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+            if slow_ptr == fast_ptr:
+                return True
+        return False
+        
